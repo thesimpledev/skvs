d727c73d93e8e25e71cda92289ee0ea76594a4e7
sstanton <steven@thesimpledev.com>
2025-11-08 12:43:16 -0500

    test: Add ProssessMessage test

--- internal/skvs/skvs_test.go
+++ internal/skvs/skvs_test.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"log/slog"
 	"testing"
+
+	"github.com/thesimpledev/skvs/internal/protocol"
 )
 
 func TestNew(t *testing.T) {
@@ -17,5 +19,35 @@ func TestNew(t *testing.T) {
 
 	if app.skvs == nil {
 		t.Error("skvs is nil and should not be ")
+	}
+}
+
+func TestProcessMessage(t *testing.T) {
+	tests := []struct {
+		name  string
+		frame []byte
+		err   bool
+	}{
+		{
+			name:  "valid frame",
+			frame: protocol.DtoToFrame(protocol.FrameDTO{Cmd: protocol.CMD_SET, Key: "key", Value: []byte("value")}),
+			err:   false,
+		},
+		{
+			name:  "invalid frame size",
+			frame: make([]byte, 100),
+			err:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			app := newTestApp()
+
+			_, err := ProcessMessage(app, tt.frame)
+			if (err != nil) != tt.err {
+				t.Errorf("ProcessMessage() error = %v, wantErr %v", err, tt.err)
+			}
+		})
 	}
 }
